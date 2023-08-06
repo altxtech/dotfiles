789608bf5589b3aaf97ef2e2e4856bb4d570c358
atiagosoares <atiagosoares@gmail.com>
2023-08-05 22:04:26 -0300

    Added my nvim dotfiles

---
+++ nvim/lua/rooke/plugins.lua
@@ -0,0 +1,54 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  -- Telescope
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  -- Color Themes
+  use { "catppuccin/nvim", as = "catppuccin" }
+  use "rebelot/kanagawa.nvim"
+
+  -- Game
+  use 'ThePrimeagen/vim-be-good'
+
+  -- Treesitter
+  use ('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' }) 
+
+  -- LSP Zero Setup
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v1.x',
+	  requires = {
+	    -- LSP Support
+	    {'neovim/nvim-lspconfig'},             -- Required
+	    {'williamboman/mason.nvim'},           -- Optional
+	    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+	    -- Autocompletion
+	    {'hrsh7th/nvim-cmp'},         -- Required
+	    {'hrsh7th/cmp-nvim-lsp'},     -- Required
+	    {'hrsh7th/cmp-buffer'},       -- Optional
+	    {'hrsh7th/cmp-path'},         -- Optional
+	    {'saadparwaiz1/cmp_luasnip'}, -- Optional
+	    {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+	    -- Snippets
+	    {'L3MON4D3/LuaSnip'},             -- Required
+	    {'rafamadriz/friendly-snippets'}, -- Optional
+	  }
+	}
+  -- Barbar	  
+  -- These optional plugins should be loaded directly because of a bug in Packer lazy loading
+  use 'nvim-tree/nvim-web-devicons' -- OPTIONAL: for file icons
+  use 'lewis6991/gitsigns.nvim' -- OPTIONAL: for git status
+  use 'romgrk/barbar.nvim'
+  -- Nvim tree
+  use 'nvim-tree/nvim-tree.lua'
+end)
